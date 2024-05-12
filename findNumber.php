e9f310897a4ceaa7f0168a5b930c708829910966
Semyon Karasev <karasev.semyon00@mail.ru>
2024-05-12 16:55:35 +0300

    Добавление формы для будущей игры

--- findNumber.php
+++ findNumber.php
@@ -5,7 +5,7 @@
 */
 $message = "Угадай число в диапазоне от 0 до 100!";
 
-if(isset($_POST["checkDate"])){
+if(isset($_POST["checkDate"])){ //Если данные были отправлены на сервер
 
 }
 ?>
@@ -15,4 +15,12 @@ if(isset($_POST["checkDate"])){
         <title>Игра угадай число</title>
         <h1>Hello world</h1>
     </head>
+    <body>
+        <h2><?php echo $message;?></h2>
+        <form method "post">
+            <input type = "text" value ="<?php echo $number ?>" name = "number"/>
+            <input type = "submit" name = "checkDate" value ="Угадать"/><br/>
+            <input type = "hidden" value ="hidden_number" value ="<?php echo $hidden_number ?>"/>
+        </form>
+    </body>
 </html>
